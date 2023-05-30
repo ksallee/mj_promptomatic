3d4b17b63d84a6305aa68fcbd085963ae0769880
Kevin Sallee <kevin.sallee@gpltech.com>
2023-05-29 23:45:15 -0600

    and now?

--- src/routes/api/generate-prompt/+server.js
+++
@@ -1,8 +0,0 @@
-import { MIDJOURNEY_EXPLANATION_TINY} from "$lib/constants";
-import {PROMPT_FILLER_EXPLANATION} from "$lib/constants.js";
-import { error, json } from '@sveltejs/kit';
-
-
-export async function POST({request, fetch}) {
-  const { prompt_text, api_key, num_replies = 1 } = await request.json();
-
